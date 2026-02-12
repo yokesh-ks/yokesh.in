3f4de5ba883f9355b9e9df4b8a14395484472e13
yokesh-ks <ksyokesh98@gmail.com>
2026-02-12 08:37:46 +0530

    fix: Update Explore My Work link to point to projects page instead of empty hash

--- src/components/blocks/about-us-section/about-us-page.tsx
+++ src/components/blocks/about-us-section/about-us-page.tsx
@@ -30,7 +30,7 @@ const AboutUs = ({ stats }: { stats: Stat[] }) => {
             asChild
             className='group relative w-fit overflow-hidden rounded-full text-base before:absolute before:inset-0 before:rounded-[inherit] before:bg-[length:250%_250%,100%_100%] before:bg-[linear-gradient(45deg,transparent_25%,rgba(255,255,255,0.5)_50%,transparent_75%,transparent_100%)] before:bg-[position:200%_0,0_0] before:bg-no-repeat before:transition-[background-position_0s_ease] before:duration-1000 hover:before:bg-[position:-100%_0,0_0] has-[>svg]:px-6 dark:before:bg-[linear-gradient(45deg,transparent_25%,rgba(0,0,0,0.2)_50%,transparent_75%,transparent_100%)]'
           >
-            <a href='#'>
+            <a href='/projects'>
               Explore My Work
               <ArrowRightIcon className='transition-transform duration-200 group-hover:translate-x-0.5' />
             </a>
